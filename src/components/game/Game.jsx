2292158f45f215103b9e9886e5f43455d44a2482
vinit-churi <vinitchuri@hotmail.com>
2023-02-24 02:25:01 +0530

    before making breaking changes

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,59 +1,86 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import "./game.css";
 import Player from "./Player";
 import Ball from "./Ball";
 const Game = () => {
-  const [playerOne, setPlayerOne] = useState({x: 0,y : 0});
-  // const [playerTwo, setPlayerTwo] = useState({x : 0,y : 0});
-  function movePlayer(event){
-    if(event.key === "ArrowUp"){
-      setPlayerOne(state => {
-        return {...state,y : Number(state["y"]) + 1}
-      })
-    }else if(event.key === "ArrowDown"){
-      setPlayerOne(state => {
-        return {...state,y : Number(state["y"]) - 1}
-      })
-      console.log("arrow down");
-    }else if(event.key === "ArrowLeft"){
-      setPlayerOne(state => {
-        return {...state,x : Number(state["x"]) + 1}
-      })
-      console.log("arrow left");
-    }else if(event.key === "ArrowRight"){
-      setPlayerOne(state => {
-        return {...state,x : Number(state["x"]) - 1}
-      })
-      console.log("arrow right");
-    }
-    
-    if(event.key === "w"){
-      console.log(event.key);
-    }else if(event.key === "s"){
-      console.log(event.key);
-    }else if(event.key === "a"){
-      console.log(event.key);
-    }else if(event.key === "d"){
-      console.log(event.key);
-    }
-  }
-  useEffect(() => {
-    window.addEventListener('keydown', movePlayer, false);
-    return function() {
-      return window.removeEventListener('keydown', movePlayer, false);
-    } 
-  },[])
-  return (
-    <>
-      <div id="board" onKeyUp={movePlayer}>
-        <div id="goal-left"></div>
-        <Player position={{x : playerOne.x, y: playerOne.y}} player={"one"} />
-        <Ball/>
-        <Player style={{}} player={"two"} />
-        <div id="goal-right"></div>
-      </div>
-    </>
-  )
-}
+    const [playerOne, setPlayerOne] = useState({ x: 0, y: 0 });
+    const [arrowKeysStatus, setArrowKeysStatus] = useState([
+        false,
+        false,
+        false,
+        false,
+    ]);
+    const [asdfKeysStatus, setasdfKeysStatus] = useState([
+        false,
+        false,
+        false,
+        false,
+    ]);
+    // const [playerTwo, setPlayerTwo] = useState({x : 0,y : 0});
+    function movePlayer(event) {
+        event.preventDefault();
+        if (event.key === "ArrowUp") {
+            setPlayerOne((state) => {
+                return { ...state, y: Number(state["y"]) - 5 };
+            });
+        }
+        if (event.key === "ArrowUp" && event.key === "ArrowRight") {
+            console.log("working");
+        }
+        if (event.key === "ArrowDown") {
+            setPlayerOne((state) => {
+                return { ...state, y: Number(state["y"]) + 5 };
+            });
+            console.log("arrow down");
+        }
+        if (event.key === "ArrowLeft") {
+            setPlayerOne((state) => {
+                return { ...state, x: Number(state["x"]) - 5 };
+            });
+            console.log("arrow left");
+        }
+        if (event.key === "ArrowRight") {
+            setPlayerOne((state) => {
+                return { ...state, x: Number(state["x"]) + 5 };
+            });
+            console.log("arrow right");
+        }
 
-export default Game
+        if (event.key === "w") {
+            console.log(event.key);
+        }
+        if (event.key === "s") {
+            console.log(event.key);
+        }
+        if (event.key === "a") {
+            console.log(event.key);
+        }
+        if (event.key === "d") {
+            console.log(event.key);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("keydown", movePlayer, false);
+        window.addEventListener("keyup", movePlayer, false);
+        return function () {
+            window.removeEventListener("keydown", movePlayer, false);
+            window.addEventListener("keyup", movePlayer, false);
+        };
+    }, []);
+    return (
+        <>
+            <div id="board" onKeyUp={movePlayer}>
+                <div id="goal-left"></div>
+                <Player
+                    position={{ x: playerOne.x, y: playerOne.y }}
+                    player={"one"}
+                />
+                <Ball />
+                {/* <Player style={{}} player={"two"} /> */}
+                <div id="goal-right"></div>
+            </div>
+        </>
+    );
+};
+
+export default Game;
